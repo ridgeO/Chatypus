efaa7d7be86cfdc99f19459478a2358a97d3ed62
Ridge <rto@MBAir.local>
2017-06-22 15:42:13 -0700

    hook up to gifted-chat

--- App.js
+++ App.js
@@ -7,14 +7,14 @@ import { StackNavigator } from 'react-navigation';
 import SignIn from './components/SignIn.js';
 import SignUp from './components/SignUp.js';
 import Channels from './components/Channels.js';
-import Chat from './components/Chat.js'
+import ChatV2 from './components/ChatV2.js'
 
 const RootNavigator = StackNavigator(
   {
     Login: { name: 'SignIn', screen: SignIn },
     SignUp: { name: 'SignUp', screen: SignUp },
     Channels: { name: 'Channels', screen: Channels },
-    Chat: { name: 'Chat', screen: Chat }
+    Chat: { name: 'Chat', screen: ChatV2 }
   },
   { headerMode: 'screen' }
 );
---
+++ components/ChatV2.js
@@ -0,0 +1,89 @@
+'use strict'
+import React, { Component } from 'react';
+import {
+  Text,
+  TextInput,
+  TouchableHighlight,
+  StatusBar,
+  KeyboardAvoidingView,
+  FlatList,
+  View
+} from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import firebaseApp from './firebaseConfig.js';
+import styles from './styles.js';
+
+class ChatV2 extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: `${navigation.state.params.channelName} Chat`,
+    headerStyle: {backgroundColor: '#1E90FF'},
+    headerTitleStyle: styles.chatTopperNav,
+    headerBackTitleStyle: styles.chatBackNav
+  });
+
+  constructor(props) {
+    super(props);
+    var FirebaseDB = firebaseApp.database();
+    var FirebaseUser = firebaseApp.auth().currentUser.uid;
+    this.messagesRef = FirebaseDB.ref(`messages/${this.props.navigation.state.params.channelKey}`);
+    this.state = {
+      user: FirebaseUser,
+      loading: true,
+      channelKey: this.props.navigation.state.params.channelKey,
+      channelName: this.props.navigation.state.params.channelName,
+    }
+  }
+
+  componentDidMount(){
+    // start listening for firebase updates
+    this.listenForMessages(this.messagesRef);
+  }
+
+  listenForMessages(messagesRef){
+    //Get data from firebase and update listview accordingly
+    messagesRef.on('value', (dataSnapshot) => {
+      var messages = [];
+      dataSnapshot.forEach((child) => {
+        messages.push({
+          _id: child.key,
+          text: child.val().msg,
+          createdAt: child.val().createdAt,
+          user: { _id: child.val().user }
+        });
+      });
+      console.log(messages);
+
+      this.setState({
+        messages:messages
+      });
+    });
+  }
+
+  addMessage(message = {}){
+    console.log(message[0]);
+    this.messagesRef.push({
+      user: message[0].user._id,
+      msg: message[0].text,
+      createdAt: Date.now()
+    })
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <StatusBar
+          barStyle="light-content"
+        />
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={this.addMessage.bind(this)}
+          user={{
+            _id: this.state.user
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+export default ChatV2;
